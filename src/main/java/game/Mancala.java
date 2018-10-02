267704ffbe3f58ddcecff840081d3085e452ca58
Miranda Spoelstra <mspoelstra@sogyo.nl>
2018-10-02 08:57:48 +0200

    Mancala game class toegevoegd om te spelen

--- src/main/java/game/Mancala.java
+++ src/main/java/game/Mancala.java
@@ -1,10 +1,122 @@
-package components;
+package game;
+
+import java.util.Scanner;
+
+import components.Cup;
+import components.Kalaha;
+import components.Player;
 
 public class Mancala {
+	private Player firstPlayer;
+	private Cup startingCup;
+	boolean gameOver;
+
+	public Mancala() {
+		this.startingCup = new Cup();
+		this.firstPlayer = getStartingCup().getOwner();
+		this.gameOver = false;
+	}
 
 	public static void main(String[] args) {
+		Mancala game = new Mancala();
+		// MancalaGUI gui = new MancalaGUI(game);
+		// gui.display();
+		game.playerTurn();
+	}
 
+	private void playerTurn() {
+
+		try (Scanner userInput = new Scanner(System.in)) {
+			while (!gameOver()) {
+				displayGame();
+				gameOverCheck();
+
+				String player = "one";
+				if (!getFirstPlayer().hasTurn()) {
+					player = "two";
+				}
+				System.out.println("Player " + player + " can make a move now");
+				makeMove(userInput);
+			}
+		}		
+	}
+
+	protected boolean gameOver() {
+		return this.gameOver;
+	}
+
+	private void gameOverCheck() {
+		Cup currentCup = getStartingCup();
+
+		if (!getFirstPlayer().hasTurn()) {
+			currentCup = (Cup) currentCup.findHole(8);
+		}
+
+		if (currentCup.gameEnded()) {
+			setGameOver(true);
+			displayWinner();
+		}
+	}
+
+	private void setGameOver(boolean gameOver) {
+		this.gameOver = gameOver;
+	}
+
+	private void displayWinner() {
+		Kalaha winner = getStartingCup().getWinner();
+		String winningPlayer = "Player 2";
+		int score = winner.getStones();
+
+		if (winner.getOwner().equals(getFirstPlayer())) {
+			winningPlayer = "Player 1";
+		}
+		System.out.println("Game over!");
+		System.out.println(winningPlayer + " is the winner with a score of " + score);
+	}
+
+	public Player getFirstPlayer() {
+		return firstPlayer;
+	}
+
+	protected Cup getStartingCup() {
+		return startingCup;
+	}
+
+	public void makeMove(Scanner userInput) {
+		System.out.println("Make your move by choosing from cup 1 - 6.");
+		int cupNr = userInput.nextInt();
+
+		if(cupNr > 6) {
+			cupNr = cupNr % 6;
+		}
 		
-		
+		if (!getFirstPlayer().hasTurn()) {
+			cupNr += 7;
+		}
+		Cup currentCup = (Cup) getStartingCup().findHole(cupNr);
+		currentCup.giveAwayStones();
+		System.out.println("Your move was done succesfully.");
+	}
+
+	private void displayGame() {
+		System.out.println();
+		displayCups(-13, -8);
+		displayCups(1, 6);
+		System.out.println();
+	}
+
+	private void displayCups(int start, int finish) {
+		if (finish < 0) {
+			System.out.print(getStartingCup().findHole(Math.abs(start) + 1).getStones() + " | ");
+		} else {
+			System.out.print("    ");
+		}
+		for (int i = start; i <= finish; i++) {
+			System.out.print(getStartingCup().findHole(Math.abs(i)).getStones() + " | ");
+		}
+		if (finish > 0) {
+			System.out.print(getStartingCup().findHole(finish + 1).getStones());
+		}
+		System.out.println();
 	}
 }
--- src/test/java/components/CupTest.java
+++ src/test/java/components/CupTest.java
@@ -7,144 +7,140 @@ import org.junit.jupiter.api.Test;
 class CupTest {
 
 	@Test
-	void testCupStones() {
+	void testCupSetup() {
 		Cup cup = new Cup();
 		assertEquals(4, cup.getStones(), "The cup should have the starting amount of stones");
-	}
-
-	@Test
-	void getPlayer() {
-		Cup cup = new Cup();
-		assertTrue(cup.getOwner().hasTurn(), "The owner of the first cup should be the current player.");
-	}
-
-	@Test
-	void getOpponentTurn() {
-		Cup cup = new Cup();
-		assertFalse(cup.getOwner().getOpponent().hasTurn(), "The opponent should not be the current player.");
-	}
-
-	@Test
-	void nextHoleStones() {
-		Cup cup = new Cup();
 		assertEquals(4, cup.getNextHole().getStones(), "The next cup should have the starting amount of stones");
 	}
 
 	@Test
-	void emptyAfterGiveAwayStones() {
+	void testPlayerTurn() {
 		Cup cup = new Cup();
-		cup.giveAwayStones();
-		assertEquals(0, cup.getStones(), "The cup should be empty");
+		assertTrue(cup.getOwner().hasTurn(), "The owner of the first cup should be the current player.");
+		assertFalse(cup.getOwner().getOpponent().hasTurn(), "The opponent should not be the current player.");
 	}
 
 	@Test
-	void unableToMakeMove() {
+	void testGiveAwayStones() {
+		Cup cup = new Cup();
+		cup.giveAwayStones();
+		assertEquals(0, cup.getStones(), "The cup should be empty");
+		assertEquals(5, cup.getNextHole().getStones(), "After give away, the next cup should have an extra stone");
+		assertEquals(4, cup.findHole(6).getStones(), "The give away should stop after the stones are up");
+	}
+
+	@Test
+	void testImpossibleMove() {
 		Cup cup = new Cup();
 		cup.giveAwayStones();
 		cup.giveAwayStones();
 		assertEquals(5, cup.getNextHole().getStones(), "The next cup should only have 1 extra stone");
 	}
-
+	
 	@Test
-	void nextCupAddStone() {
+	void testMoveOnOpponentCup() {
 		Cup cup = new Cup();
-		cup.giveAwayStones();
-		assertEquals(5, cup.getNextHole().getStones(), "After give away, the next cup should have an extra stone");
+		Cup currentCup = (Cup) cup.findHole(8);
+		currentCup.giveAwayStones();
+		assertEquals(4, currentCup.getStones(), "Should still have stones, because not a cup of the current player");
+		assertTrue(cup.getOwner().hasTurn(), "The turn should not have switched");
 	}
 
 	@Test
-	void findHole() {
+	void testFindHole() {
 		Cup cup = new Cup();
 		assertEquals(4, cup.findHole(5).getStones(), "This hole should exist and have the starting amount of stones");
 	}
 
 	@Test
-	void findFirst() {
+	void testFullCircle() {
 		Cup cup = new Cup();
 		assertEquals(cup, cup.findHole(15), "The 15th hole should be the first cup");
 	}
-	
+
 	@Test
-	void kalahaExists() {
+	void testKalahaExists() {
 		Cup cup = new Cup();
-		assertTrue(cup.findHole(7) instanceof Kalaha, "This hole should be a kalaha");
+		Hole kalaha = cup.findHole(7);
+		assertTrue(kalaha instanceof Kalaha, "This hole should be a kalaha");
 	}
 
 	@Test
-	void giveAwayNotToFar() {
-		Cup cup = new Cup();
-		cup.giveAwayStones();
-		assertEquals(4, cup.findHole(6).getStones(), "The give away should stop after the stones are up");
-	}
-
-	@Test
-	void opponentHasCupsToo() {
-		Cup cup = new Cup();
-		assertFalse(cup.findHole(8).getOwner().hasTurn(), "The owner of the cup on the other side of the board should not be the current player");
-	}
-
-	@Test
-	void switchTurnAfterGiveAway() {
+	void testSwitchTurnAfterMove() {
 		Cup cup = new Cup();
 		cup.giveAwayStones();
 		assertFalse(cup.getOwner().hasTurn(), "The turn should have passed to the opponent");
 	}
 
 	@Test
-	void findOpposingCup() {
+	void testTurnDoesnotSwitch() {
 		Cup cup = new Cup();
-		Cup current = (Cup) cup.findHole(3);
-		Cup opposite = current.findOppositeCup();
+		Cup currentCup = (Cup) cup.findHole(3);
+		currentCup.giveAwayStones();
+		assertTrue(currentCup.getOwner().hasTurn(), "The player should have another turn, because ended in kalaha");
+	}
+
+	@Test
+	void testFindOpposingCup() {
+		Cup cup = new Cup();
+		Cup currentCup = (Cup) cup.findHole(3);
+		Cup opposite = currentCup.findOppositeCup();
 		assertFalse(opposite.getOwner().hasTurn(), "The owner of this cup should not be the current player");
 	}
 
 	@Test
-	void hitOpponent() {
+	void testHitOpponent() {
 		Cup cup = new Cup();
 		cup.getNextHole().setStones(0);
 		cup.setStones(1);
 		cup.giveAwayStones();
 		assertEquals(0, cup.findHole(12).getStones(), "The opposite cup should be empty");
-	}
-
-	@Test
-	void giveStonesPastKalaha() {
-		Cup cup = new Cup();
-		Cup current = (Cup) cup.findHole(4);
-		current.giveAwayStones();
-		assertEquals(5, current.findHole(4).getStones(), "The hole after the kalaha should have an extra stone");
-	}
-	
-	@Test
-	void kalahaDifferentOwner() {
-		Cup cup = new Cup();
-		assertNotEquals(cup.findHole(7).getOwner(), cup.findHole(14).getOwner(), "The kalaha's should have different owners");
-	}
-	
-	@Test
-	void giveToKalaha() {
-		Cup cup = new Cup();
-		cup.getNextHole().setStones(0);
-		cup.setStones(1);
-		cup.giveAwayStones();
 		assertEquals(5, cup.findHole(7).getStones(), "The current player's kalaha should have 5 stones");
+		assertEquals(0, cup.findHole(14).getStones(), "The opponents kalaha should still be empty");
 	}
 
 	@Test
-	void gameNotOver() {
+	void testGiveStonesPastKalaha() {
+		Cup cup = new Cup();
+		Cup currentCup = (Cup) cup.findHole(4);
+		currentCup.giveAwayStones();
+		assertEquals(1, cup.findHole(7).getStones(), "The kalaha should have 1 stone");
+		assertEquals(5, currentCup.findHole(4).getStones(), "The hole after the kalaha should have an extra stone");
+	}
+
+	@Test
+	void testKalahaDifferentOwner() {
+		Cup cup = new Cup();
+		assertNotEquals(cup.findHole(7).getOwner(), cup.findHole(14).getOwner(),
+				"The kalaha's should have different owners");
+	}
+
+	@Test
+	void testStonesFullCircle() {
+		Cup cup = new Cup();
+		Cup currentCup = (Cup) cup.findHole(6);
+		currentCup.setStones(8);
+		currentCup.giveAwayStones();
+		assertEquals(0, cup.findHole(14).getStones(), "The opposing kalaha should still be empty");
+		assertEquals(5, cup.findHole(15).getStones(), "The cup after the opposing kalaha should have the last stone");
+		assertEquals(4, cup.findHole(16).getStones(), "This cup should still have the starting amount of stones");
+	}
+
+	@Test
+	void testGameNotOver() {
 		Cup cup = new Cup();
 		cup.giveAwayStones();
 		assertFalse(cup.gameEnded(), "Game should not be over");
 	}
 
 	@Test
-	void gameEnded() {
+	void testGameEnded() {
 		Cup cup = new Cup();
 		cup.setStones(0);
-		Cup current = cup;
+		Cup currentCup = cup;
 		for (int i = 0; i < 6; i++) {
-			current = (Cup) current.getNextHole();
-			current.setStones(0);
+			currentCup = (Cup) currentCup.getNextHole();
+			currentCup.setStones(0);
 		}
 		assertTrue(cup.gameEnded(), "Game should be over");
 	}
